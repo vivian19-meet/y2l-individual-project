6bf31c400fb88007086008810c9b14a2ce3db28d
aboud20-meet <aboud20@meet.mit.edu>
2019-01-28 23:11:30 +0200

    d

--- app.py
+++ app.py
@@ -22,16 +22,55 @@ def un_info(id):
     return render_template('univ_info.html',u=u,add=add)
 @app.route('/test')
 def test():
+    
     return render_template('test.html')
+@app.route('/lang')
+def lag():
+    global ll
+    ll=True
+    return render_template('test.html',ll=ll)
+@app.route('/math')
+def math():
+    global mm
+    mm=True
+    return render_template('test.html',mm=mm)
+@app.route('/hhh')
+def hhh():
+    global hh
+    hh=True
+    return render_template('test.html',hh=hh)
+@app.route('/mu')
+def music():
+    global mu
+    mu=True
+    return render_template('test.html',mu=mu)
+@app.route('/kk')
+def sport():
+    global kk
+    kk=True
+    return render_template('test.html',kk=kk)
+@app.route('/so')
+def social():
+    global so
+    so=True
+    return render_template('test.html',so=so)
+@app.route('/ii')
+def introvert():
+    global ii
+    ii=True
+    return render_template('test.html',ii=ii)
+
 
 @app.route('/adminC', methods=["GET",'POST'])
 def adminC():
     global add
+    global tt
     if request.method=="POST":
         if(code == request.form["code"]):
             add=True
             return redirect('/')     
-    return render_template('adminC.html')
+        tt=True
+    return render_template('adminC.html', add=add)
 
 @app.route('/123456787654321345678534678hsdanarlsknrlibigjk',methods=["GET",'POST'])
 def addd():
